d018d37f081bafd90d5f4a3d28d225370cc3ba5b
Eric Berkow <eric.berkow@gmail.com>
2017-10-23 20:33:41 -0600

    heroku fixes

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
 
+$dbUrl = parse_url(env("CLEARDB_DATABASE_URL"));
 return [
     /*
       |--------------------------------------------------------------------------
@@ -36,12 +37,11 @@ return [
         ],
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => isset($dbUrl["host"]) ? $dbUrl["host"] : env("DB_HOST"),
+            'port' => isset($dbUrl["port"]) ? $dbUrl["port"] : env("DB_PORT"),
+            'database' => isset($dbUrl["path"]) ? ltrim($dbUrl["path"], '/') : env("DB_DATABASE"),
+            'username' => isset($dbUrl["user"]) ? $dbUrl["user"] : env("DB_USERNAME"),
+            'password' => isset($dbUrl["pass"]) ? $dbUrl["pass"] : env("DB_PASSWORD"),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
